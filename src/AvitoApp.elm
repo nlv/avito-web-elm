a656b7ff069686c881906c86a995db397626b5e1
Leon Nikitin <nlv@lab321.ru>
2021-08-02 21:40:58 +0600

    Добавляем обработку событий в таблице

--- src/AvitoApp.elm
+++ src/AvitoApp.elm
@@ -3,34 +3,29 @@ module AvitoApp exposing (main)
 import List as List
 import Tuple as Tuple
 
-import Html exposing (p, text)
+import Browser
+import Html exposing (text, span)
+import Html.Events exposing (onClick)
 
 import Bootstrap.CDN as CDN
 import Bootstrap.Grid as Grid
 import Bootstrap.Table as Table
 import Bootstrap.Form.Input as Input
-import Html.Attributes exposing (attribute)
-import Bootstrap.Carousel exposing (Msg)
 
-view : Model -> Html.Html ()
-view model = 
-    Grid.container []
-        [ CDN.stylesheet
-        , avitoTable model
-        ]
-
+import Maybe exposing (andThen)
 
 {- ------ -}
 
-type CellMessage = 
-    CellUpdate Int String
+type CellMsg = 
+      CellUpdate Int String
+    | CellFocus Int
 
-type alias CellInfo = { name : String, normal : String -> List (Html.Html ()), edit : String -> List (Html.Html CellMessage) }
+type alias CellInfo = { name : String, normal : String -> List (Html.Html CellMsg), edit : String -> List (Html.Html CellMsg) }
 
 textCell : String -> Int -> CellInfo  
-textCell n i = { name = n, normal = \t -> [text t], edit = \t -> [Input.text  [Input.value t, Input.onInput (CellUpdate i)]]  }
+textCell n i = { name = n, normal = \t -> [span [ onClick (CellFocus i)] [text t]], edit = \t -> [Input.text  [Input.small, Input.value t, Input.onInput (CellUpdate i)]]  }
 
-type CellStatus = CellNormal -- | CellEditable
+type CellStatus = CellNormal | CellEditable
 
 type alias Cell = { value : String, status : CellStatus}
 
@@ -44,30 +39,49 @@ type alias Model = {
 initCell : Cell
 initCell = { value = "", status = CellNormal }
 
+initCell2 : Cell
+initCell2 = { value = "", status = CellEditable }
+
 initModel : Model
 initModel = {
       cellsInfo = [textCell "col1" 0, textCell "col2" 1, textCell "col3" 2]
-    , cells = [initCell, initCell, initCell]
+    , cells = [initCell, initCell2, initCell]
     }
 
 -- type Message = MCellMessage CellMessage
 
-main : Html.Html ()
-main = view initModel
+update : CellMsg -> Model -> Model
+update action model =
+  case action of
+    CellUpdate i str -> model
+    CellFocus i -> model
+    -- Array.get i model.cells |> andThen
 
 
-avitoTable : Model -> Html.Html ()
+main : Program () Model CellMsg
+main = Browser.sandbox { init = initModel, update = update, view = view }
+
+view : Model -> Html.Html CellMsg
+view model = 
+    Grid.container []
+        [ CDN.stylesheet
+        , avitoTable model
+        ]
+ 
+avitoTable : Model -> Html.Html CellMsg
 avitoTable model = 
     let 
         headP = List.map (\i -> [text i.name]) model.cellsInfo
         cellsP = List.map viewCell (List.map2 Tuple.pair model.cellsInfo model.cells)
         viewCell (info, cell) = case cell.status of
                                  CellNormal -> info.normal cell.value 
+                                 CellEditable -> info.edit cell.value
     in
     Table.table {
       options = [ Table.bordered, Table.hover ]
-    , thead = Table.simpleThead (List.map (\h -> Table.th [] h) headP) 
+    -- , thead = Table.simpleThead (List.map (\h -> Table.th [] h) headP) 
+    , thead = Table.simpleThead (List.map (Table.th []) headP) 
     , tbody = Table.tbody [] [
-            Table.tr [] (List.map (\c -> Table.td [] c) cellsP)-- [ Table.td [] cellsP ]
+            Table.tr [] (List.map (Table.td []) cellsP)
         ]
     }
