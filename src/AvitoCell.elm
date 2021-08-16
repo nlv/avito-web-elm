30e9494eb3f90d3e6e058fce699621441ff42f45
Leon Nikitin <nlv@lab321.ru>
2021-08-16 17:19:40 +0600

    Добавили контроль фокуса на уровне табилцы и переход вправо по стрелке и enter. Надо отдельно различать текущую клетку, в фокусе она или нет. Если не в фокусе, тогда только обрабатывать нажатие кнопок

--- src/AvitoCell.elm
+++ src/AvitoCell.elm
@@ -1,52 +1,74 @@
-module AvitoCell exposing (Msg, Model, update, view, text, setValue)
+module AvitoCell exposing (Msg, Model, update, view, text, setValue, focusIn, savingFocusOut, cancelingFocusOut)
 
 import Task
 import Browser.Dom exposing (focus, Error)
 
 import Html exposing (Html, Attribute, td, input)
-import Html.Events exposing (onClick, onInput, on, onBlur, keyCode)
-import Html.Attributes exposing (id, value, style)
+import Html.Events exposing (onClick, onInput, on, onBlur, keyCode, onFocus, on)
+import Html.Attributes exposing (id, value, style, autofocus)
 
 import Json.Decode as Json
-
-
 
 type Msg = 
       SetValue String
 
     | Input String
-    | SetEditable 
+    -- | SetEditable 
     | SetNormal
+    | Click
 
     | FocusResult (Result Error ())
-    | KeyUp Int
-
+    -- | KeyUp Int
 
 type Status = Normal | Editable String
 
 type alias Model = {
       value   : String  
-    , key     : String
+    -- , key     : String
     , status  : Status
-    , normal  : String -> Html.Html Msg
-    , edit    : String -> Html.Html Msg    
-    , focusId : String
+    , normal  : String -> String -> List (Html.Html Msg)
+    , edit    : String -> String -> List (Html.Html Msg)
+    , focusId : String -> String
     }
 
-text : String -> String -> Model
-text key val0 = let focusId = "cell-editable-input-" ++ key in 
+-- text : String -> String -> Model
+text : String -> Model
+-- text key val0 = let focusId = "cell-editable-input-" ++ key in 
+text val0 = let focusId key = "cell-editable-input-" ++ key in 
   { value = val0
-  , key = key
+  -- , key = key
   , status = Normal
-  , normal = \val -> td [style "border" "solid 1px black", style "width" "600px", onClick SetEditable] [Html.text val]
-  , edit = \val -> td [style "border" "solid 1px black", style "width" "600px"] [
-        input [id focusId, value val, onInput Input, onKeyUp KeyUp, onBlur SetNormal, style "width" "580px", style "border" "none"] []
+  -- , normal =  -- \val -> td [style "border" "solid 1px black", style "width" "600px", onClick SetEditable] [Html.text val]
+  , normal = \key val -> [
+        input [id (focusId key), value val{-, onInput Input, onKeyUp KeyUp, onBlur SetNormal-}, style "width" "580px", style "border" "none"] []
+    ]    
+  , edit = \key val -> [
+        input [id (focusId key), value val, onInput Input, autofocus True{-, onKeyUp KeyUp-}{-, onBlur SetNormal-}, style "width" "580px", style "border" "none"] []
     ]  
   , focusId = focusId
   }
 
 setValue : Model -> String -> Model
 setValue model val = {model | value = val}
+
+focusIn : Model -> Model
+focusIn model = 
+  let newStatus = 
+        case model.status of
+          Normal -> Editable (model.value)
+          Editable str -> Editable str
+  in {model | status = newStatus}
+
+savingFocusOut : Model -> Model
+savingFocusOut model = {model | status = Normal}  
+
+cancelingFocusOut : Model -> Model
+cancelingFocusOut model = 
+  let newValue = 
+        case model.status of
+          Normal -> model.value
+          Editable str -> str
+  in {model | value = newValue}    
 
 update : Msg -> Model -> (Model, Cmd Msg, Bool)
 update action model =
@@ -55,7 +77,7 @@ update action model =
 
     Input str -> ({ model | value = str}, Cmd.none, False)
 
-    SetEditable -> ({ model | status = Editable model.value}, Task.attempt FocusResult (focus model.focusId), False)
+    -- SetEditable -> ({ model | status = Editable model.value}, Task.attempt FocusResult (focus model.focusId), False)
 
     SetNormal -> ({ model | status = Normal }, Cmd.none, True)
 
@@ -64,8 +86,10 @@ update action model =
                 Err _ -> (model, Cmd.none, False)
                 Ok _ -> (model, Cmd.none, False)
 
-    KeyUp 27 -> (calceledModel model, Cmd.none, False)
-    KeyUp _ ->  (model, Cmd.none, False)          
+    Click -> (model, Cmd.none, False)
+
+    -- KeyUp 27 -> (calceledModel model, Cmd.none, False)
+    -- KeyUp _ ->  (model, Cmd.none, False)          
 
 calceledModel : Model -> Model
 calceledModel model = 
@@ -74,11 +98,11 @@ calceledModel model =
                         _ -> model.value
       in { model | status = Normal, value = newValue}
 
-view : Model -> Html Msg
-view model = 
+view : String -> Model -> List (Html Msg)
+view key model = 
   case model.status of
-    Normal       -> model.normal model.value
-    Editable _   -> model.edit model.value
+    Normal       -> model.normal key model.value
+    Editable _   -> model.edit key model.value
 
 onKeyUp : (Int -> msg) -> Attribute msg
 onKeyUp tagger =
--- src/AvitoTable.elm
+++ src/AvitoTable.elm
@@ -6,7 +6,14 @@ import Array2D as Array2D
 import Utils exposing (..)
 
 import Html exposing (Html, text, table, tr, th, td)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, on)
+import Html.Attributes exposing (tabindex, style)
+import Browser.Dom exposing (focus, Error)
+import Task
+
+import Json.Decode as Json
+import Keyboard.Event exposing (KeyboardEvent, decodeKeyboardEvent)
+import Keyboard.Key as Key exposing (Key(..))
 
 import AvitoCell as Cell
 
@@ -18,9 +25,14 @@ type Msg =
 
     | CellMsg Int Int Cell.Msg
 
+    | CellClick Int Int
+
+    | HandleKeyboardEvent KeyboardEvent
+    | FocusResult (Result Error ())
+
 type alias CellInfo = { 
     name : String
-  , mkModel : String -> String -> Cell.Model  
+  , mkModel : String -> Cell.Model  
   }
 
 type alias Model = {
@@ -28,7 +40,7 @@ type alias Model = {
     , cells : Array2D.Array2D Cell.Model
     , rowsCnt : Int
     , colsCnt : Int
-    -- , focused : Maybe (Int, Int)
+    , focused : Maybe (Int, Int)
     }
 
 initModel : (Array.Array String) -> Array.Array (Array.Array String) -> Model
@@ -42,7 +54,7 @@ initModel hs ds =
   , cells = dataToCells Cell.text cellsInfo ds2 
   , rowsCnt = Array.length ds + 1
   , colsCnt = colsCnt
-  -- , focused = Nothing
+  , focused = Nothing
   }    
 
 getData : Model -> Array.Array (Array.Array String)
@@ -55,6 +67,15 @@ update : Msg -> Model -> (Model, Cmd Msg, Maybe (Array.Array (Array.Array String
 update action model =
   case action of
     SetData ds -> (setData model ds, Cmd.none, Nothing)
+
+    CellClick i j -> 
+      let newCells = 
+            case model.focused of
+              Just (iF, jF) -> model.cells |> updateArray2D iF jF Cell.savingFocusOut |> updateArray2D i j Cell.focusIn
+              Nothing       -> model.cells |> updateArray2D i j Cell.focusIn
+          newTask = Maybe.map (\c -> Task.attempt FocusResult <| focus (c.focusId (mkCellKey i j))) (Array2D.get i j newCells) |> Maybe.withDefault Cmd.none    
+          newModel = {model | cells = newCells, focused = Just (i, j)}
+      in (newModel,  newTask, Just <| getData newModel)
 
     CellMsg i j msg -> 
       let res = Maybe.map (Cell.update msg) (Array2D.get i j model.cells)
@@ -78,19 +99,53 @@ update action model =
         Nothing -> (model, Cmd.none, Nothing)
 
     InsertRow i -> 
-      let newCells = insertRow i model.cells (emptyCellsRow i model.cellsInfo)
-          newModel = {model | cells = newCells, rowsCnt = model.rowsCnt + 1}
+      let newCells = insertRow i model.cells (emptyCellsRow model.cellsInfo)
+          newFocused = 
+            case model.focused of
+               Just (iF, jF) -> if iF == i then Just (iF + 1, jF) else Just (iF, jF)
+               Nothing       -> Nothing
+          newModel = {model | cells = newCells, rowsCnt = model.rowsCnt + 1, focused = newFocused}
+          newTask = 
+            case newFocused of
+              Just (iF, jF) -> Maybe.map (\c -> Task.attempt FocusResult <| focus (c.focusId (mkCellKey iF jF))) (Array2D.get iF jF newCells) |> Maybe.withDefault Cmd.none    
+              _ -> Cmd.none          
       in
-      (newModel, Cmd.none, Just <| getData newModel)
+      (newModel, newTask, Just <| getData newModel)
 
     DeleteRow i -> 
-      if i == model.rowsCnt - 1 then
+      if (Debug.log "deleting: " i) == model.rowsCnt - 1 then
         (model, Cmd.none, Nothing)
       else
         let newCells = Array2D.deleteRow i model.cells
-            newModel = {model | cells = newCells, rowsCnt = model.rowsCnt - 1}
+            newCells2 = 
+              case Debug.log "focused: " model.focused of
+                Just (iF, jF) -> if iF == i then newCells |> updateArray2D iF jF Cell.focusIn else newCells
+                _             -> newCells
+            a = Debug.log "newCells2: " <| Array2D.indexedMap (\is js _ -> (is, js)) newCells2
+            newModel = {model | cells = newCells2, rowsCnt = model.rowsCnt - 1}
+            newTask = 
+              case model.focused of
+                Just (iF, jF) -> Maybe.map (\c -> Task.attempt FocusResult <| focus (c.focusId (mkCellKey iF jF))) (Array2D.get iF jF newCells) |> Maybe.withDefault Cmd.none    
+                _ -> Cmd.none
         in
-        (newModel, Cmd.none, Just <| getData newModel)
+        (newModel, newTask, Just <| getData newModel)
+
+    -- HandleKeyboardEvent event -> if (Debug.log "keyeveny:" event == event) then (model, Cmd.none, Nothing) else (model, Cmd.none, Nothing)
+    HandleKeyboardEvent event -> 
+      case Debug.log "model.focused: " <| model.focused of
+        Just (iF, jF) -> 
+          case Debug.log "keyCode: " <| event.keyCode of
+            Enter -> moveFocus Right model
+            Key.Right -> moveFocus Right model
+            _ -> (model, Cmd.none, Nothing)
+        _ -> (model, Cmd.none, Nothing)
+          
+          
+
+    FocusResult result ->
+      case Debug.log "FOCUS RESULT" result of
+        Err _ -> (model, Cmd.none, Nothing)
+        Ok _ -> (model, Cmd.none, Nothing)
 
 view : Model -> Html.Html Msg
 view model = avitoTable model
@@ -101,11 +156,23 @@ avitoTable model =
         cellsInfoL = Array.toList model.cellsInfo
         headP = List.map (\i -> [text i.name]) cellsInfoL
 
-        cellsV = Array2D.indexedMap (\i j c -> Html.map (CellMsg i j) (Cell.view c)) model.cells
+        cellsV = 
+          Array2D.indexedMap 
+            (\i j c -> 
+              let attrs = 
+                    case model.focused of
+                      Nothing -> [style "border" "solid 1px black", style "width" "600px", onClick (CellClick i j)]
+                      Just (iF, jF) -> 
+                        if i == iF && j == jF 
+                        then [style "border" "solid 1px red", style "width" "600px"]
+                        else [style "border" "solid 1px black", style "width" "600px", onClick (CellClick i j)]
+              in td attrs (List.map (Html.map (CellMsg i j)) (Cell.view (mkCellKey i j) c) )
+            ) 
+            model.cells
         rows = List.range 0 ((Array2D.rows model.cells) - 1)
                 |> List.map (\i -> Array2D.getRow i cellsV |> Maybe.withDefault Array.empty |> Array.toList)
     in
-    table [] [
+    table [on "keydown" <| Json.map HandleKeyboardEvent decodeKeyboardEvent, tabindex 0] [
         Html.thead [] <| List.map (th []) headP ++ [th [] []]
       , Html.tbody [] (List.indexedMap (\i -> if i + 1 == List.length rows then avitoLastRow i else avitoRow i) (rows))
     ]
@@ -123,19 +190,48 @@ avitoRow i rowV =
 avitoLastRow : Int -> List (Html.Html Msg) -> Html.Html Msg
 avitoLastRow i rowV = Html.tr [] (rowV ++ [Html.td [] [Html.button [onClick (InsertRow i)] [text "Добавить"]]])
 
-dataToCells : (String -> String -> Cell.Model) -> Array.Array CellInfo -> Array2D.Array2D String -> Array2D.Array2D Cell.Model
+dataToCells : (String -> Cell.Model) -> Array.Array CellInfo -> Array2D.Array2D String -> Array2D.Array2D Cell.Model
 dataToCells defaultMkModel info data = 
   Array2D.indexedMap 
     (\i j val ->  
-        (Maybe.map (.mkModel) (Array.get i info) |> Maybe.withDefault defaultMkModel) (String.fromInt i ++ "-" ++ String.fromInt j) val
+        (Maybe.map (.mkModel) (Array.get i info) |> Maybe.withDefault defaultMkModel) val
 --  |> appendEmptyRow text (Array.length info)
     ) data
 
-emptyCellsRow : Int -> Array.Array CellInfo -> Array.Array Cell.Model
-emptyCellsRow i info = Array.indexedMap (\j a -> a.mkModel (mkCellKey i j) "") info
+emptyCellsRow : Array.Array CellInfo -> Array.Array Cell.Model
+emptyCellsRow info = Array.map (\a -> a.mkModel "") info
 
 appendEmptyCellsRow : Int -> Array.Array CellInfo -> Array2D.Array2D Cell.Model -> Array2D.Array2D Cell.Model
-appendEmptyCellsRow i info data = appendRow (emptyCellsRow (i + 1) info) (Cell.text (mkCellKey i 0) "") data
+appendEmptyCellsRow i info data = appendRow (emptyCellsRow info) (Cell.text "") data
 
 mkCellKey : Int -> Int -> String
 mkCellKey i j = (String.fromInt i)  ++ "-" ++ (String.fromInt j)
+
+
+type MoveFocus = Up | Right | Down | Left
+
+moveFocus : MoveFocus -> Model -> (Model, Cmd Msg, Maybe (Array.Array (Array.Array String)))
+moveFocus move model = 
+      case Debug.log "model.focused: " <| model.focused of
+        Just (iF, jF) -> 
+          let (newIF, newJF) = newFocusIJ move model iF jF
+              newFocused = Just (newIF, newJF)
+              newCells = model.cells |> updateArray2D newIF newJF Cell.focusIn 
+              newTask = 
+                case Debug.log "newFocused" newFocused of
+                  Just (iF2, jF2) -> Maybe.map (\c -> Task.attempt FocusResult <| focus (c.focusId (Debug.log "newTask" <| mkCellKey iF2 jF2))) (Array2D.get iF jF newCells) |> Maybe.withDefault Cmd.none    
+                  _ -> Cmd.none              
+              newModel = {model | cells = newCells, focused = newFocused}
+          in
+          (newModel, newTask, Just <| getData newModel)
+        _ -> (model, Cmd.none, Nothing)
+
+newFocusIJ : MoveFocus -> Model -> Int -> Int -> (Int, Int)
+newFocusIJ move model i j = 
+  case move of
+    Right -> 
+      let newI = if j == model.colsCnt - 1 then (if i == model.rowsCnt - 1 then 0 else i + 1) else i
+          newJ = if j == model.colsCnt - 1 then 0 else j + 1
+      in (newI, newJ)
+    
+    _ -> (i, j)
